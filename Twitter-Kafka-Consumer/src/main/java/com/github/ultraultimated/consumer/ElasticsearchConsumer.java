32e9cfe61e2ce588efd6326c3c9032b5369848f6
ultraultimated <neelp1543@gmail.com>
2020-09-30 23:52:16 -0400

    Added Synchronous Commits to elasticsearch.
    Signed-off-by: ultraultimated <neelp1543@gmail.com>

--- Twitter-Kafka-Consumer/src/main/java/com/github/ultraultimated/consumer/ElasticsearchConsumer.java
+++ Twitter-Kafka-Consumer/src/main/java/com/github/ultraultimated/consumer/ElasticsearchConsumer.java
@@ -1,11 +1,17 @@
 package com.github.ultraultimated.consumer;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -17,22 +23,75 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 public class ElasticsearchConsumer implements ElasticsearchProperties {
+    private static final String BOOTSTRAP_SEVER_CONFIG = "127.0.0.1:9092";
     public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(ElasticsearchConsumer.class.getName());
         RestHighLevelClient highLevelClient = createClient();
-        String staticStringJson = "{\"foo\":\"bar\"}";
-        IndexRequest indexRequest = new IndexRequest(
-                "twitter", "tweets"
-        ).source(staticStringJson, XContentType.JSON);
 
-        IndexResponse indexResponse = highLevelClient.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        logger.info("ID of current json string is " + id);
+
+        KafkaConsumer<String, String> consumer = createKafkaConsumer("tweets");
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                String id = extractIdFromJson(record.value());
+                String staticStringJson = record.value();
+                IndexRequest indexRequest = new IndexRequest(
+                        "twitter", "tweets", id
+                ).source(staticStringJson, XContentType.JSON);
+
+                IndexResponse indexResponse = highLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+                String responseId = indexResponse.getId();
+                logger.info("ID of current json string is " + responseId);
+
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                logger.info("Committing now...");
+                consumer.commitSync();
+                logger.info("Committed...!");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         //close the client
-        highLevelClient.close();
+        //highLevelClient.close();
+
+    }
+
+    private static String extractIdFromJson(String value) {
+        return JsonParser.parseString(value).getAsJsonObject().get("id_str").getAsString();
+    }
+
+    public static KafkaConsumer<String, String> createKafkaConsumer(String topic) {
+        String group = "tweets_group";
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SEVER_CONFIG);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, group);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        List<String> topics = Arrays.asList(topic);
+        consumer.subscribe(topics);
+        return consumer;
+
 
     }
 
